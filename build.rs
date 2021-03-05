92ca01a2315696619f910e92e49f8044a1d687c1
Manuel Schölling <manuel.schoelling@gmx.de>
2021-03-05 11:17:09 +0100

    Update to opendht v2.1.0

--- build.rs
+++ build.rs
@@ -1,11 +1,12 @@
 extern crate cc;
 
 fn main() {
-    println!("cargo:rustc-flags=-l gnutls -l nettle -l argon2");
+    println!("cargo:rustc-flags=-lopendht -lgnutls -lssl -lcrypto -lnettle -lpthread -ljsoncpp -largon2 -lhttp_parser");
 
     cc::Build::new()
         .file("src/wrapper.cpp")
         .cpp(true)
+        .flag_if_supported("-std=c++14")
         .compile("dht-wrapper");
 
     println!("cargo:rustc-link-lib=static=dht-wrapper");
